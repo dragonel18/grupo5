718bdb27927b0f2a1edb6346c16ef328bf51892a
dragonel18 <dragonel18@gmail.com>
2025-05-15 23:01:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,2 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+streamlit run dashboard_tarea.py
